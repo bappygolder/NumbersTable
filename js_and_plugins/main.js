e72a19986161cb00fc5c017831c0137bc2e04d5b
Bappy Golder <bups.co@gmail.com>
2016-11-15 12:53:47 +1100

    show and hide help option on scroll added
    used even handeller of the sticky pluggin:
    https://github.com/garand/sticky

--- js_and_plugins/main.js
+++ js_and_plugins/main.js
@@ -4,4 +4,15 @@ $(".animsition").animsition();
 //stick the input section to the top
 $("header").sticky();
 
+//hide & show help option when user scrolls
+$("header").on('sticky-start', function(){
+	"use strict";
+	$(".header_wrapper .question_mark").css("display", "none");
+});
+
+$("header").on('sticky-end', function(){
+	"use strict";
+	$(".header_wrapper .question_mark").css("display", "block");
+});
+
 //show tooltip with help options
